39acd7e20de82edd428be014baf13cc47257381b
Rachwan <rachwan.harb2023@gmail.com>
2024-02-03 09:43:06 +0200

    Connect the app with Google Analytics

--- confluence/src/index.js
+++ confluence/src/index.js
@@ -5,8 +5,12 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals.js";
 import { HelmetProvider } from "react-helmet-async";
 import { UserProvider } from "./UseContext/UserContext.js";
+import ReactGA from 'react-ga4';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+ReactGA.initialize("G-Y8KGYE10GV");
+
 root.render(
   <React.StrictMode>
     <UserProvider>
@@ -17,7 +21,14 @@ root.render(
   </React.StrictMode>
 );
 
+const SendAnalytics = ()=> {
+  ReactGA.send({
+    hitType: "pageview",
+    page: window.location.pathname,
+  });
+}
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(SendAnalytics);
