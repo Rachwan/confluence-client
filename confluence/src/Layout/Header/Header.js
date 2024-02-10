9b9d673f40156d40184a14334a3c2b6a2f65096a
Rachwan <rachwan.harb2023@gmail.com>
2024-02-10 20:47:52 +0200

    Active links

--- confluence/src/Layout/Header/Header.js
+++ confluence/src/Layout/Header/Header.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import styles from "./Header.module.css";
 import loginIcon from "../../Assets/Icons/log-in.png";
-import { Link } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 import confluence from "../../Assets/Icons/confluence.svg";
 import edit from "../../Assets/Icons/pen-to-square-solid.svg";
 import { UserContext } from "../../UseContext/UserContext.js";
@@ -12,6 +12,7 @@ import Swal from "sweetalert2";
 function Header({ home = false }) {
   const { user, setUser } = useContext(UserContext);
   const navigate = useNavigate();
+  const location = useLocation();
 
   const handleLogout = async () => {
     try {
@@ -53,21 +54,41 @@ function Header({ home = false }) {
         </Link>
         <nav className={styles.nav}>
           <ul className={styles.linklist}>
-            <Link to="/" className={styles.link}>
+            <NavLink
+              to="/"
+              className={`${styles.link} ${
+                location.pathname === "/" ? styles.active__link : ""
+              }`}>
               <li>Home</li>
-            </Link>
-            <Link to="/influencers" className={styles.link}>
+            </NavLink>
+            <NavLink
+              to="/influencers"
+              className={`${styles.link} ${
+                location.pathname === "/influencers" ? styles.active__link : ""
+              }`}>
               <li>Influencers</li>
-            </Link>
-            <Link to="/pricing" className={styles.link}>
+            </NavLink>
+            <NavLink
+              to="/pricing"
+              className={`${styles.link} ${
+                location.pathname === "/pricing" ? styles.active__link : ""
+              }`}>
               <li>Pricing</li>
-            </Link>
-            <Link to="/about" className={styles.link}>
+            </NavLink>
+            <NavLink
+              to="/about"
+              className={`${styles.link} ${
+                location.pathname === "/about" ? styles.active__link : ""
+              }`}>
               <li>About</li>
-            </Link>
-            <Link to="/contact" className={styles.link}>
+            </NavLink>
+            <NavLink
+              to="/contact"
+              className={`${styles.link} ${
+                location.pathname === "/contact" ? styles.active__link : ""
+              }`}>
               <li>Contact</li>
-            </Link>
+            </NavLink>
           </ul>
         </nav>
         <div className={styles.auth}>
@@ -96,21 +117,7 @@ function Header({ home = false }) {
                 </Link>
               </div>
             </>
-          ) :  (
-          //   <>
-          //     <div
-          //       className={`${styles.logout__wrapper} ${styles.button} ${styles.join__as__brand}`}
-          //       onClick={handleLogout}>
-          //       <svg
-          //         xmlns="http://www.w3.org/2000/svg"
-          //         viewBox="0 0 512 512"
-          //         className={styles.logout__icon}>
-          //         <path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z" />
-          //       </svg>
-          //       <span className={styles.logout}>Logout</span>
-          //     </div>
-          //   </>
-          // ) : (
+          ) : (
             <div className={styles.buttons}>
               <Link to={"/dashboard"}>
                 <div
