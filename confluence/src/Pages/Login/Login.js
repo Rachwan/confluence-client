4c2da5c84b7ec558346318cb1fb5c5df6a1b190b
Rachwan <rachwan.harb2023@gmail.com>
2024-03-01 11:10:43 +0200

    modified login file

--- confluence/src/Pages/Login/Login.js
+++ confluence/src/Pages/Login/Login.js
@@ -77,25 +77,27 @@ function Login() {
         formData
       );
       if (response.data) {
-        await fetchUserData();
-        Swal.fire({
-          title: "Welcome Back",
-          text: "Login successfully!",
-          icon: "success",
-        });
-        if (response.data.data.role === "admin") {
-          setTimeout(() => {
-            window.location.href = `${process.env.REACT_APP_DASHBOARD_LINK}/`;
-          }, 1000);
-        } else if (response.data.data.role === "influencer") {
-          setTimeout(() => {
-            window.location.href = `${process.env.REACT_APP_DASHBOARD_LINK}/influencer-details`;
-          }, 1000);
-        } else {
-          navigate(`/`, {
-            replace: true,
-          });
-        }
+        // await fetchUserData();
+        setUser(response.data);
+        // Swal.fire({
+        //   title: "Welcome Back",
+        //   text: "Login successfully!",
+        //   icon: "success",
+        // });
+        // if (response.data.data.role === "admin") {
+        //   setTimeout(() => {
+        //     window.location.href = `${process.env.REACT_APP_DASHBOARD_LINK}/`;
+        //   }, 1000);
+        // } else if (response.data.data.role === "influencer") {
+        //   setTimeout(() => {
+        //     window.location.href = `${process.env.REACT_APP_DASHBOARD_LINK}/influencer-details`;
+        //   }, 1000);
+        // } else {
+        //   navigate(`/`, {
+        //     replace: true,
+        //   });
+        // }
+        console.log(response.data);
       }
     } catch (error) {
       if (error.response && error.response.status === 401) {
